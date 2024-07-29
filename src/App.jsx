3e3a9aedf4b3ca86a614e4b62e98e008e58e6bb8
AnkitSinghSenwal <ankitsinghsenwal2001@gmail.com>
2024-07-29 12:09:14 +0530

    Remove: Extra comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 import Card from './Component/Card.jsx'
 import Data from './assets/Data.jsx'
 import Header from './Component/Header.jsx'
 
 function App() {
-  // const [count, setCount] = useState(0)
-
-
   return (
     <div className='page'>
       <Header/>
